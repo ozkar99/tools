4abf1a1cfcdbdb017d6b6c91bf7a0dba37a869f3
Oscar Moreno Garza <oscarmg99@gmail.com>
2018-05-02 12:34:00 -0500

    adds unsafe_eval for vue.js

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -24,9 +24,5 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
 Rails.application.config.content_security_policy do |policy|
-  if Rails.env.development?
-    policy.script_src :self, :https, :unsafe_eval
-  else
-    policy.script_src :self, :https
-  end
+    policy.script_src :self, :https, :unsafe_eval # add unsafe_eval for vue.js
 end
