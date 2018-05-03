3aa4af2fbe8e0cb0906892898f97a3b82823a570
ozkar99 <oscarmg99@gmail.com>
2018-05-03 03:07:27 -0500

    adds docker service to travis

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+IMAGE_NAME="${DOCKER_NAME}:${TRAVIS_COMMIT}"
+
 # push to docker repository
 docker login -u "$DOCKER_USERNAME"  -p "$DOCKER_PASSWORD"
-docker build -t ozkar99/tools .
-docker push ozkar99/tools
+docker build -t "$IMAGE_NAME" .
+docker push "$IMAGE_NAME"
 
 # send post to webhook here too notify server to pull latest image
 # curl -X POST 
