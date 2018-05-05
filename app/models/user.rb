0b819b65f8802e060bd3a767086cc1d90c5ea0f8
ozkar99 <oscarmg99@gmail.com>
2018-05-05 13:40:41 -0500

    adds encryption key to env for tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :two_factor_authenticatable,
          :rememberable, :trackable, :validatable,
-         otp_secret_encryption_key: Rails.application.credentials.totp_encryption_key
+         otp_secret_encryption_key: ENV['RAILS_OTP_SECRET_ENCRYPTION_KEY'] || Rails.application.credentials.totp_encryption_key
 
   after_create do |user|
     user.otp_required_for_login = true
