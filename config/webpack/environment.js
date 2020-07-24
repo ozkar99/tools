11d6f76ba37a2ca4e5b4485217c16ff38c791829
Oscar Moreno Garza <ozkar@ozkar.org>
2020-07-23 23:05:41 -0700

    Upgrades to webpack 5 (#44)
    * webpack update
    
    * fixes missing vueloader plugin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
 const vuecompiler = require('./vue-compiler')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 environment.config.merge(vuecompiler)
 environment.loaders.append('vue', vue)
+environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin())
 module.exports = environment
